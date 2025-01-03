fef214b2bdb8fa1ae0d8266df6bb57b1fc3daba9
ArditBeqaj <arditi.b75@gmail.com>
2025-01-03 01:15:12 +0100

    update(Components): Added more Tiles.

--- src/Pages/CompanyPage/CompanyPage.tsx
+++ src/Pages/CompanyPage/CompanyPage.tsx
@@ -5,7 +5,7 @@ import { getCompanyProfile } from "../../api";
 import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import Tile from "../../Components/Tile/Tile";
-
+import Spinner from "../../Components/Spinner/Spinner";
 
 interface Props {}
 
@@ -25,13 +25,19 @@ const CompanyPage = (props: Props) => {
     <>
       {company ? (
         <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
-          <Sidebar/>
-          <CompanyDashboard>
-          <Tile title="Company Name" subTitle={company.companyName}></Tile> 
+          <Sidebar />
+          <CompanyDashboard ticker={ticker!}>
+            <Tile title="Company Name" subTitle={company.companyName}></Tile>
+            <Tile title="Price" subTitle={company.price.toString()}></Tile>
+            <Tile title="Sector" subTitle={company.sector}></Tile>
+            <Tile title="DCF" subTitle={company.dcf.toString()}></Tile> 
+            <p className="bg-white shadow rounded text-medium text-gray-900 p-3 mt-1 m-4">
+              {company.description}
+            </p>
           </CompanyDashboard>
         </div>
       ) : (
-        <div> Company not found</div>
+        <Spinner/>
       )}
     </>
   );
