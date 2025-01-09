79455eb6b8fbc6dd945b059e151072437872c43b
ArditBeqaj <arditi.b75@gmail.com>
2025-01-09 15:52:17 +0100

    filler: Migration

---
+++ api/Migrations/20250109143229_RenameProperty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CopmanyName",
+                table: "Stock",
+                newName: "CompanyName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CompanyName",
+                table: "Stock",
+                newName: "CopmanyName");
+        }
+    }
+}
