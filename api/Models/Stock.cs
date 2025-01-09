35bd5c5a18b7c34c9c366368c7736d2fe893acb8
ArditBeqaj <arditi.b75@gmail.com>
2025-01-09 15:52:45 +0100

    fix: Name fix

--- api/Models/Stock.cs
+++ api/Models/Stock.cs
@@ -10,7 +10,7 @@ namespace api.Models
     {
         public int Id { get; set; }
         public string Symbol { get; set; } = string.Empty;
-        public string CopmanyName { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
         [Column(TypeName ="decimal(18,2)")]
         public decimal Purchase { get; set; }
         [Column(TypeName ="decimal(18,2)")]
