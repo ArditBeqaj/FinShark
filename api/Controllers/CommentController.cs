0fa1bbddd2e941334de5a8090338900afd3136b3
ArditBeqaj <arditi.b75@gmail.com>
2025-01-15 21:01:34 +0100

    add: Implemented the Create & Update methods for Comment Controller .

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.DTOs.Comment;
 using api.Interfaces;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
@@ -13,10 +14,11 @@ namespace api.Controllers
     public class CommentController : ControllerBase
     {
         private readonly ICommentRepository _commentRepo;
-
-        public CommentController(ICommentRepository commentRepo)
+        private readonly IStockRepository _stockRepo;
+        public CommentController(ICommentRepository commentRepo, IStockRepository stockRepo)
         {
             _commentRepo = commentRepo;
+            _stockRepo = stockRepo;
         }
         
         [HttpGet]
@@ -31,6 +33,28 @@ namespace api.Controllers
             var comment = await _commentRepo.GetByIdAsync(id);
             if(comment == null){
                 return NotFound();
+            }
+            return Ok(comment.ToCommentDTO());
+        }
+
+        [HttpPost("{stockId}")]
+        public async Task<IActionResult> Create([FromRoute] int stockId, CreateCommentDTO commentDTO)
+        {
+            if(!await _stockRepo.StockExist(stockId)){
+                return BadRequest("Stack does not exist");
+            }
+            var commentModel = commentDTO.ToCommentFromCreate(stockId);
+            await _commentRepo.CreateAsync(commentModel);
+            return CreatedAtAction(nameof(GetById), new {id = commentModel.Id}, commentModel.ToCommentDTO());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDTO updateDTO){
+            var comment = await _commentRepo.UpdateAsync(id, updateDTO.ToCommentFromUpdate());
+            if(comment == null){
+                NotFound("Comment not found");
             }
             return Ok(comment.ToCommentDTO());
         }
