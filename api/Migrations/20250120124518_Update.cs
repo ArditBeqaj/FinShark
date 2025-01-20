8ce6ceb3e797277384b1b867d81b67cbee29a9bf
ArditBeqaj <arditi.b75@gmail.com>
2025-01-20 14:22:32 +0100

    Filler

--- api/Migrations/20250120124518_Update.cs
+++ api/Migrations/20250120124518_Update.cs
@@ -5,24 +5,18 @@
 namespace api.Migrations
 {
     /// <inheritdoc />
-    public partial class RenameProperty : Migration
+    public partial class Update : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.RenameColumn(
-                name: "CopmanyName",
-                table: "Stock",
-                newName: "CompanyName");
+
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.RenameColumn(
-                name: "CompanyName",
-                table: "Stock",
-                newName: "CopmanyName");
+
         }
     }
 }
--- api/Service/TokenService.cs
+++ api/Service/TokenService.cs
@@ -17,7 +17,7 @@ namespace api.Service
         private readonly SymmetricSecurityKey _key;
         public TokenService(IConfiguration config){
             _config = config;
-            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:signingKey"]));
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:SigningKey"]));
         }
         public string CreateToken(AppUser user)
             {
