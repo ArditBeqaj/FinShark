d62142298aea95edeec68641fff37e569adc44bf
ArditBeqaj <arditi.b75@gmail.com>
2025-01-24 04:13:53 +0100

    add/fix: Portfolio

--- frontend/src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
+++ frontend/src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
@@ -1,21 +1,31 @@
 import React, { SyntheticEvent } from 'react'
 import DeletePortfolio from '../DeletePortfolio/DeletePortfolio';
 import { Link } from 'react-router-dom';
+import { PortfolioGet } from '../../../Models/Portfolio';
 
 interface Props{
-    portfolioValue: string;
+    portfolioValue: PortfolioGet;
     onPortfolioDelete: (e : SyntheticEvent) => void;
 }
 
 const CardPortfolio = ({portfolioValue, onPortfolioDelete}: Props) => {
   return (
-    <div className="flex flex-col w-full p-8 space-y-4 text-center rounded-lg shadow-lg md:w-1/3">
-    <Link to={`/company/${portfolioValue}/company-profile`} className="pt-6 text-xl font-bold">{portfolioValue}</Link>
-    <DeletePortfolio
-      portfolioValue={portfolioValue}
-      onPortfolioDelete={onPortfolioDelete}
-    />
+    <div className="flex flex-col w-full p-6 text-center rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105 md:w-1/3 bg-gray-100">
+
+    <Link
+      to={`/company/${portfolioValue.symbol}/company-profile`}
+      className="pt-4 text-2xl font-bold text-gray-800 hover:text-blue-600 transition-colors"
+    >
+      {portfolioValue.symbol}
+    </Link>
+    <div className="mt-4">
+      <DeletePortfolio
+        portfolioValue={portfolioValue.symbol}
+        onPortfolioDelete={onPortfolioDelete}
+      />
+    </div>
   </div>
+  
   )
 }
 
